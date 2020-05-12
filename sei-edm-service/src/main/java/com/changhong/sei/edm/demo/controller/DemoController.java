f52f1be59310b17e0700bfcae7884ae326b3e389
Vision.Mac <chao2.ma@changhong.com>
2020-05-12 23:51:44 +0800

    优化url

--- sei-edm-service/src/main/java/com/changhong/sei/edm/demo/controller/DemoController.java
+++ sei-edm-service/src/main/java/com/changhong/sei/edm/demo/controller/DemoController.java
@@ -2,19 +2,27 @@ package com.changhong.sei.edm.demo.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RefreshScope
 @Controller
 @Api(value = "示例演示", tags = "示例演示")
 public class DemoController {
+    //引入配置
+    @Value("${sei.edm.base-url:none}")
+    private String baseUrl;
 
     /**
      * 演示页
      */
     @ApiOperation("演示")
     @GetMapping(value = "/demo")
-    public String uploadPage() {
+    public String uploadPage(Model model) {
+        model.addAttribute("baseUrl", baseUrl);
         return "demo.html";
     }
 }
