41f6827e27825d528c558f4d856e646f2699d146
冯华 <hua.feng@changhong.com>
2020-08-26 19:03:09 +0800

    开票日期截取，去掉时间，只保留日期

--- sei-edm-service/src/main/java/com/changhong/sei/edm/ocr/service/impl/DefaultCharacterReaderServiceImpl.java
+++ sei-edm-service/src/main/java/com/changhong/sei/edm/ocr/service/impl/DefaultCharacterReaderServiceImpl.java
@@ -153,8 +153,12 @@ public class DefaultCharacterReaderServiceImpl implements CharacterReaderService
                         }
                         // 开票金额(不含税) arr[4]
                         invoiceVO.setAmount(arr[4]);
-                        // 开票日期 arr[5]
-                        invoiceVO.setDate(arr[5]);
+                        // 开票日期 arr[5]，截取日期
+                        String invoiceDate = arr[5];
+                        if (StringUtils.isNoneBlank(invoiceDate) && invoiceDate.length() > 8) {
+                            invoiceDate = invoiceDate.substring(0, 8);
+                        }
+                        invoiceVO.setDate(invoiceDate);
                         // 校验码
                         invoiceVO.setCheckCode(arr[6]);
                         if (arr.length > 7) {
@@ -176,8 +180,12 @@ public class DefaultCharacterReaderServiceImpl implements CharacterReaderService
                         invoiceVO.setCategory("增值税电子普通发票");
                         // 总金额（含税）价税合计
                         invoiceVO.setTotalAmount(arr[3]);
-                        // 开票日期 arr[5]
-                        invoiceVO.setDate(arr[2]);
+                        // 开票日期 arr[2]
+                        String invoiceDate = arr[2];
+                        if (StringUtils.isNoneBlank(invoiceDate) && invoiceDate.length() > 8) {
+                            invoiceDate = invoiceDate.substring(0, 8);
+                        }
+                        invoiceVO.setDate(invoiceDate);
                         // 校验码
                         invoiceVO.setCheckCode(arr[4]);
                         result = JsonUtils.toJson(invoiceVO);
